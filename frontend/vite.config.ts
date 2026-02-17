a2179a51918ab61223ce504e6c83a61318d1bd84
Yuzuru_Orensu <korrawitaoongern@gmail.com>
2026-02-18 00:14:12 +0700

    Fix : pre-prod -> dev env
    just change vite proxy target URL to localhost:8000 to make it work when develop locally by npm run dev and fastapi dev app/main.py.
    in contrast, this dev branch will now not be able to run on docker compose (unless change it back, why so? you can just switch to pre-prod and it's all good)

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
         proxy: {
             // เมื่อเรียก /api ใน frontend จะถูกส่งไปหา backend โดยอัตโนมัติ
             '/api': {
-                target: 'http://backend:8000', // ชื่อ service ใน docker-compose
+                target: 'http://localhost:8000',
                 changeOrigin: true,
             }
         }
